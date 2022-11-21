1fd6a0f8990b2a5d35007a5857cb73473be234a3
TheDarkSword <TheDarkSword01@gmail.com>
2022-11-21 14:57:53 +0100

    [Fix] Party members follow leader when he joins in a lobby

--- velocity/src/main/java/it/hemerald/basementx/velocity/listeners/PlayerListener.java
+++ velocity/src/main/java/it/hemerald/basementx/velocity/listeners/PlayerListener.java
@@ -4,8 +4,13 @@ import com.velocitypowered.api.event.PostOrder;
 import com.velocitypowered.api.event.Subscribe;
 import com.velocitypowered.api.event.connection.DisconnectEvent;
 import com.velocitypowered.api.event.connection.PostLoginEvent;
+import com.velocitypowered.api.event.player.ServerConnectedEvent;
+import com.velocitypowered.api.proxy.Player;
+import it.hemerald.basementx.api.party.Party;
 import it.hemerald.basementx.velocity.BasementVelocity;
 import org.redisson.api.RAtomicLong;
+
+import java.util.Optional;
 
 public class PlayerListener {
 
@@ -30,4 +35,18 @@ public class PlayerListener {
         velocity.getUserDataManager().cacheUser(event.getPlayer());
     }
 
+    @Subscribe
+    public void onServerSwitch(ServerConnectedEvent event) {
+        if(!event.getServer().getServerInfo().getName().contains("_lobby")) return;
+        Optional<Party> optional = velocity.getTogether().getPartyManager().getParty(event.getPlayer());
+        if(optional.isEmpty()) return;
+        Party party = optional.get();
+        if(!party.getLeader().equals(event.getPlayer().getUsername())) return;
+
+        for (String memberName : party.getMembers()) {
+            Optional<Player> optionalMember = velocity.getServer().getPlayer(memberName);
+            if(optionalMember.isEmpty()) continue;
+            optionalMember.get().createConnectionRequest(event.getServer()).fireAndForget();
+        }
+    }
 }
